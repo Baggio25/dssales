7c355a8b0da0dad86a64fef4679661a5d5673043
Rodrigo Baggio <rodrigo.baggio.si@gmail.com>
2021-08-12 14:19:40 -0300

    Add SalesTable

---
+++ frontweb/src/components/sales-table/index.tsx
@@ -0,0 +1,64 @@
+import React from 'react';
+
+import './styles.css';
+
+const SalesTable = () => {
+  return (
+    <div className="sales-table-container base-card">
+      <h3 className="sales-table-title">Vendas recentes</h3>
+      <table className="sales-table">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Data</th>
+            <th>Gênero</th>
+            <th>Categoria</th>
+            <th>Loja</th>
+            <th>Forma de pagamento</th>
+            <th>Total</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>#341</td>
+            <td>07/11/2020</td>
+            <td>Feminino</td>
+            <td>Roupas e Acessórios</td>
+            <td>Uberlândia</td>
+            <td>Crédito</td>
+            <td>R$ 540,00</td>
+          </tr>
+          <tr>
+            <td>#341</td>
+            <td>07/11/2020</td>
+            <td>Feminino</td>
+            <td>Roupas e Acessórios</td>
+            <td>Uberlândia</td>
+            <td>Crédito</td>
+            <td>R$ 540,00</td>
+          </tr>
+          <tr>
+            <td>#341</td>
+            <td>07/11/2020</td>
+            <td>Feminino</td>
+            <td>Roupas e Acessórios</td>
+            <td>Uberlândia</td>
+            <td>Crédito</td>
+            <td>R$ 540,00</td>
+          </tr>
+          <tr>
+            <td>#341</td>
+            <td>07/11/2020</td>
+            <td>Feminino</td>
+            <td>Roupas e Acessórios</td>
+            <td>Uberlândia</td>
+            <td>Crédito</td>
+            <td>R$ 540,00</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default SalesTable;
